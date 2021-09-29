36c247ae568ae6264d5827b7f4c64139509c6e5f
heesoo-kim97 <heesoo0323@gmail.com>
2021-09-29 18:20:44 +0000

    Completed century-from-year exercise.

---
+++ century-from-year/century.js
@@ -0,0 +1,14 @@
+/* exported centuryFromYear centuryFromYear2 */
+function centuryFromYear(year) {
+  let count = 0;
+  if (year % 100 === 0) {
+    count = year / 100;
+  } else {
+    count = ((year / 100) - (year % 100) / 100) + 1;
+  }
+  return count;
+}
+
+function centuryFromYear2(year) {
+  return Math.ceil(year / 100);
+}
