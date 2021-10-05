db6657f701cad24394228801ec3ccabc873f5c2b
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-05 05:48:50 +0000

    added another way to add two digits of a integer.

--- add-two-digit/twoDigit.js
+++ add-two-digit/twoDigit.js
@@ -1,7 +1,11 @@
-/* exported addTwoDigits */
+/* exported addTwoDigits addTwoDigits2 */
 
 function addTwoDigits(n) {
   const arr = (n).toString(10).split('').map(Number);
 
   return arr[0] + arr[1];
+}
+
+function addTwoDigits2(n) {
+  return n % 10 + Math.floor(n / 10);
 }
