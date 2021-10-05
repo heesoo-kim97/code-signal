a33eaf85102394a34bd9338ccfd602b44ff4c785
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-05 06:12:40 +0000

    initial thought process.

--- make-array-consecutive2/consecutive2.js
+++ make-array-consecutive2/consecutive2.js
@@ -1,5 +1,14 @@
 /* exported makeArrayConsecutive2 */
 
 function makeArrayConsecutive2(statues) {
-
+  let smallestInt = 0;
+  let largestInt = 0;
+  for (let i = 0; i < statues.length; i++) {
+    if (statues[i] < statues[i + 1]) {
+      smallestInt = statues[i];
+    } else {
+      largestInt = statues[i];
+    }
+  }
+  return (largestInt - smallestInt) - (statues.length - 1);
 }
