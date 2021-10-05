314ee580aa1f7a80aeb87c20fe9f49d50bada2df
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-05 06:32:26 +0000

    Completed make-array-consecutive2.

--- make-array-consecutive2/consecutive2.js
+++ make-array-consecutive2/consecutive2.js
@@ -1,14 +1,17 @@
 /* exported makeArrayConsecutive2 */
 
 function makeArrayConsecutive2(statues) {
-  let smallestInt = 0;
-  let largestInt = 0;
+  let statuesNeeded = 0;
+  // Sort array smallest to largest
+  statues.sort((a, b) => {
+    return a - b;
+  });
+  // Iterate through array and find gaps in values
   for (let i = 0; i < statues.length; i++) {
-    if (statues[i] < statues[i + 1]) {
-      smallestInt = statues[i];
-    } else {
-      largestInt = statues[i];
+    // If there is a gap between neighboring numbers subtract higher number from lower number i.e. [3, 6] is 6 - 3. There is a gap of 3, so 2 numbers are missing i.e. 4, 5
+    if (statues[i + 1] - statues[i] > 1) {
+      statuesNeeded += statues[i + 1] - statues[i] - 1;
     }
   }
-  return (largestInt - smallestInt) - (statues.length - 1);
+  return statuesNeeded;
 }
