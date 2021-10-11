12485f89779e74f2b99e06b72bf86be089c5bb9c
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-11 07:21:50 +0000

    completed all longest strings exercise.

--- all-longest-strings/longestStrings.js
+++ all-longest-strings/longestStrings.js
@@ -1,5 +1,22 @@
 /* exported allLongestStrings */
 
-function allLongestStrings(inputArray) {
+const test = ['aba',
+  'aa',
+  'ad',
+  'vcd',
+  'aba'];
 
+function allLongestStrings(inputArray) {
+  let longest = [inputArray[0]];
+  for (let i = 1; i < inputArray.length; i++) {
+    if (inputArray[i].length === longest[0].length) {
+      longest.push(inputArray[i]);
+    } else if (inputArray[i].length > longest[0].length) {
+      longest = [inputArray[i]];
+    }
+  }
+  return longest;
 }
+
+// eslint-disable-next-line no-console
+console.log('final result: ', allLongestStrings(test));
