d88260c9aaa52545eeb861cc38f3b3e88b01ffc2
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-17 02:51:10 +0000

    added a different function that adds string 9 per n value.

--- largest-number/largest.js
+++ largest-number/largest.js
@@ -1,5 +1,13 @@
-/* exported largestNumber */
+/* exported largestNumber largestNumber2 largestNumber3 */
 
 function largestNumber(n) {
   return Math.pow(10, n) - 1;
+}
+
+function largestNumber2(n) {
+  var num = '';
+  for (var i = 0; i < n; i++) {
+    num += '9';
+  }
+  return Number(num);
 }
