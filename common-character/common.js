c27e01fe4e721af03a253cde673da4aa9bc5fa0a
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-22 06:29:23 +0000

    completed common character count exercise.

--- common-character/common.js
+++ common-character/common.js
@@ -1,10 +1,16 @@
 /* exported commonCharcterCount */
 
 function commonCharcterCount(s1, s2) {
-  let firstCount = '';
-  const secondCount = '';
-  for (let i = 0; i < s2.length; i++) {
-    firstCount = s1[i];
+  let amount = 0;
+  const firstCount = s1.split('');
+  const secondCount = s2.split('');
+
+  for (let i = 0; i < firstCount.length; i++) {
+    const index = secondCount.indexOf(firstCount[i]);
+    if (index !== -1) {
+      secondCount.spice(index, 1);
+      amount += 1;
+    }
   }
-  return firstCount + secondCount;
+  return amount;
 }
