0a823c637b9db099e57cdb428b31bea993c579ca
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-22 06:22:22 +0000

    testing the function with basic return value.

--- common-character/common.js
+++ common-character/common.js
@@ -1,5 +1,10 @@
 /* exported commonCharcterCount */
 
 function commonCharcterCount(s1, s2) {
-
+  let firstCount = '';
+  const secondCount = '';
+  for (let i = 0; i < s2.length; i++) {
+    firstCount = s1[i];
+  }
+  return firstCount + secondCount;
 }
