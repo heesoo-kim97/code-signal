4b6155ba7de948a81c341c4ccd9d2915909e04f5
heesoo-kim97 <heesoo0323@gmail.com>
2021-10-22 06:29:49 +0000

    changed variable names.

--- common-character/common.js
+++ common-character/common.js
@@ -2,13 +2,13 @@
 
 function commonCharcterCount(s1, s2) {
   let amount = 0;
-  const firstCount = s1.split('');
-  const secondCount = s2.split('');
+  const arr1 = s1.split('');
+  const arr2 = s2.split('');
 
-  for (let i = 0; i < firstCount.length; i++) {
-    const index = secondCount.indexOf(firstCount[i]);
+  for (let i = 0; i < arr1.length; i++) {
+    const index = arr2.indexOf(arr1[i]);
     if (index !== -1) {
-      secondCount.spice(index, 1);
+      arr2.spice(index, 1);
       amount += 1;
     }
   }
