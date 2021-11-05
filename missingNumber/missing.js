1f49ddfd2bf94a928addfc1f4aff095daca89d7d
heesoo-kim97 <heesoo0323@gmail.com>
2021-11-05 06:13:35 +0000

    added console.

--- missingNumber/missing.js
+++ missingNumber/missing.js
@@ -14,3 +14,4 @@ console.log(findMissingNum(arr));
 // Find the missing number in given array
 // Since the array is sorted, we can solve this in one pass by looking ahead using arr[i] + 1 and comparing that to arr[i + 1].
 // If arr[i + 1] is not equal to arr[i] + 1, it means that arr[i] + 1 is the missing number.
+// console log the result
